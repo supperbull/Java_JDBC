218a945b0fa454613b40a71170c426dbe904dc02
supperbull <knicio123@gmail.com>
2016-10-25 17:05:37 +0200

    Initial commit

---
+++ src/JDBC/visual.java
@@ -0,0 +1,41 @@
+package JDBC;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by Redbullek on 2016-10-25.
+ */
+public class visual {
+    private JButton dodajButton;
+    private JButton edytujButton;
+    private JButton usunButton;
+    private JTable table1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JPasswordField passwordField1;
+    private JTextField textField4;
+
+    public visual() {
+        dodajButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        edytujButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        usunButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+}
